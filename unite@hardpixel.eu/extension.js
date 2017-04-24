a0f438cbbbcd565ad270131896d6e1ca13bc94f7
jonian <jonian@hardpixel.eu>
2017-04-24 23:37:57 +0300

    space normalization

--- unite@hardpixel.eu/extension.js
+++ unite@hardpixel.eu/extension.js
@@ -1,35 +1,35 @@
 const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const Decoration = Me.imports.decoration;
-const Buttons = Me.imports.buttons;
-const AppMenu = Me.imports.app_menu;
-const LeftBox = Me.imports.left_box;
-const TopIcons = Me.imports.top_icons;
-const ShowWindow = Me.imports.show_window;
+const Me             = ExtensionUtils.getCurrentExtension();
+const Decoration     = Me.imports.decoration;
+const Buttons        = Me.imports.buttons;
+const AppMenu        = Me.imports.app_menu;
+const LeftBox        = Me.imports.left_box;
+const TopIcons       = Me.imports.top_icons;
+const ShowWindow     = Me.imports.show_window;
 
 function init(extensionMeta) {
-	Buttons.init(extensionMeta);
-	Decoration.init(extensionMeta);
-	AppMenu.init(extensionMeta);
-	LeftBox.init(extensionMeta);
-	TopIcons.init(extensionMeta);
-	ShowWindow.init(extensionMeta);
+  Buttons.init(extensionMeta);
+  Decoration.init(extensionMeta);
+  AppMenu.init(extensionMeta);
+  LeftBox.init(extensionMeta);
+  TopIcons.init(extensionMeta);
+  ShowWindow.init(extensionMeta);
 }
 
 function enable() {
-	Buttons.enable();
-	Decoration.enable();
-	AppMenu.enable();
-	LeftBox.enable();
-	TopIcons.enable();
-	ShowWindow.enable();
+  Buttons.enable();
+  Decoration.enable();
+  AppMenu.enable();
+  LeftBox.enable();
+  TopIcons.enable();
+  ShowWindow.enable();
 }
 
 function disable() {
-	AppMenu.disable();
-	Decoration.disable();
-	Buttons.disable();
-	LeftBox.disable();
-	TopIcons.disable();
-	ShowWindow.disable();
+  AppMenu.disable();
+  Decoration.disable();
+  Buttons.disable();
+  LeftBox.disable();
+  TopIcons.disable();
+  ShowWindow.disable();
 }
