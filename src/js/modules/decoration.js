38ba6eea5c63bd3bcc1fd44258a6768024a4028e
Jonian Guveli <jonian@hardpixel.eu>
2017-08-30 21:44:24 +0300

    use gtk theme styles to undecorate csd apps in wayland

--- src/js/modules/decoration.js
+++ src/js/modules/decoration.js
@@ -1,16 +1,23 @@
 let decorationDsHandler = null;
 let decorationWindow    = null;
+let decorationStyleFile = null;
 
 function enableDecoration() {
+  decorationStyleFile = GLib.get_user_config_dir() + '/gtk-3.0/gtk.css';
   decorationDsHandler = global.display.connect('notify::focus-window', updateDecoration);
+
+  addDecorationStyles();
 }
 
 function disableDecoration() {
+  removeDecorationStyles();
+
   global.display.disconnect(decorationDsHandler);
   Mainloop.idle_add(restoreDecoration);
 
   decorationDsHandler = null;
   decorationWindow    = null;
+  decorationStyleFile = null;
 }
 
 function getXWindow(win) {
@@ -62,4 +69,31 @@ function restoreDecoration() {
     toggleXTitlebar(win._windowXID, true);
     toggleXMaximize(win);
   });
+}
+
+function addDecorationStyles() {
+  let styleContent  = decorationStyleContent();
+  let styleFilePath = extpath + '/buttons-' + buttonsPosition + '.css';
+  let styleImport   = "@import url('" + styleFilePath + "');\n\n"
+
+  GLib.file_set_contents(decorationStyleFile, styleImport + styleContent);
+}
+
+function removeDecorationStyles() {
+  let styleContent = decorationStyleContent();
+  GLib.file_set_contents(decorationStyleFile, styleContent);
+}
+
+function decorationStyleContent() {
+  let styleContent = '';
+
+  if (GLib.file_test(decorationStyleFile, GLib.FileTest.EXISTS)) {
+    let fileContent = GLib.file_get_contents(decorationStyleFile);
+
+    if (fileContent[0] == true) {
+      styleContent = fileContent[1].toString().replace(/@import.*unite@hardpixel\.eu.*css['"]\);\n\n/g, '');
+    }
+  }
+
+  return styleContent;
 }
