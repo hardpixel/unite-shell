e5e3977c65b90fe2732aa7fcabce8015e8cb58a6
Jonian Guveli <jonian@hardpixel.eu>
2017-09-21 23:44:36 +0300

    build extension.js dist

--- dist/unite@hardpixel.eu/extension.js
+++ dist/unite@hardpixel.eu/extension.js
@@ -184,10 +184,13 @@ let buttonsSizeChange = null;
 let buttonsActor      = null;
 let buttonsBox        = null;
 let focusWindow       = null;
-let buttonsPosition   = 'right';
+let buttonsItems      = null;
+let buttonsPosition   = null;
 let buttonsCallbacks  = { close: closeWindow, minimize: minimizeWindow, maximize: maximizeWindow };
 
 function enableButtons() {
+  detectButtons();
+
   Mainloop.idle_add(createButtons);
 
   buttonsSizeChange = versionCompare(Config.PACKAGE_VERSION, '3.24') < 0;
@@ -219,13 +222,14 @@ function disableButtons() {
   buttonsWmHandlers = [];
   buttonsOvHandlers = [];
   buttonsDsHandler  = null;
+  buttonsItems      = null;
   buttonsPosition   = null;
   buttonsSizeChange = null;
 
   Mainloop.idle_add(destroyButtons);
 }
 
-function createButtons() {
+function detectButtons() {
   let layout = new Gio.Settings({ schema_id: DCONF_META }).get_string('button-layout');
   let order  = layout.replace(/ /g, '').split(':');
 
@@ -233,21 +237,24 @@ function createButtons() {
     return;
   }
 
-  let buttons = collectButtons(order[1].split(','));
+  buttonsItems    = collectButtons(order[1].split(','));
+  buttonsPosition = 'right';
 
-  if (!buttons) {
-    buttons         = collectButtons(order[0].split(','));
+  if (!buttonsItems) {
+    buttonsItems    = collectButtons(order[0].split(','));
     buttonsPosition = 'left';
   }
+}
 
-  if (buttons) {
+function createButtons() {
+  if (buttonsItems) {
     buttonsActor = new St.Bin({ style_class: 'box-bin'});
     buttonsBox   = new St.BoxLayout({ style_class: 'window-buttons-box' });
 
     buttonsActor.add_actor(buttonsBox);
     buttonsActor.hide();
 
-    buttons.forEach(function (btn) {
+    buttonsItems.forEach(function (btn) {
       let callback = buttonsCallbacks[btn];
       let button   = new St.Button({ style_class: btn  + ' window-button', track_hover: true });
 
@@ -441,10 +448,11 @@ function restoreAppMenuTitle() {
 let decorationDsHandler = null;
 let decorationWindow    = null;
 let decorationStyleFile = null;
+let decorationPosition  = null;
 let decorationMaxUnmax  = null;
 
 function enableDecoration() {
-  decorationStyleFile = GLib.get_user_config_dir() + '/gtk-3.0/gtk.css';
+  decorationPosition  = buttonsPosition;
   decorationDsHandler = global.display.connect('notify::focus-window', updateDecoration);
   decorationMaxUnmax  = versionCompare(Config.PACKAGE_VERSION, '3.24') < 0;
 
@@ -460,8 +468,8 @@ function disableDecoration() {
 
   decorationDsHandler = null;
   decorationWindow    = null;
-  decorationStyleFile = null;
   decorationMaxUnmax  = null;
+  decorationPosition  = null;
 }
 
 function toggleWindowDecoration(id, hide) {
@@ -522,16 +530,22 @@ function hideDecoration(win) {
 }
 
 function addDecorationStyles() {
-  let styleContent  = decorationStyleContent();
-  let styleFilePath = extpath + '/buttons-' + buttonsPosition + '.css';
-  let styleImport   = "@import url('" + styleFilePath + "');\n"
+  decorationStyleFile = GLib.get_user_config_dir() + '/gtk-3.0/gtk.css';
 
-  GLib.file_set_contents(decorationStyleFile, styleImport + styleContent);
+  if (decorationPosition) {
+    let styleContent  = decorationStyleContent();
+    let styleFilePath = extpath + '/buttons-' + decorationPosition + '.css';
+    let styleImport   = "@import url('" + styleFilePath + "');\n"
+
+    GLib.file_set_contents(decorationStyleFile, styleImport + styleContent);
+  }
 }
 
 function removeDecorationStyles() {
   let styleContent = decorationStyleContent();
   GLib.file_set_contents(decorationStyleFile, styleContent);
+
+  decorationStyleFile = null;
 }
 
 function decorationStyleContent() {
@@ -612,7 +626,7 @@ function destroyIconsContainer() {
   }
 
   if (iconsContainer) {
-    iconsContainer.destroy();
+    iconsContainer.actor.destroy();
     iconsContainer = null;
   }
 }
