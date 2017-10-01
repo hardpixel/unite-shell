648d9083e9a7ab44c25a12fb34928607a911fe83
Jonian Guveli <jonian@hardpixel.eu>
2017-10-01 19:22:15 +0300

    fix variable in top icons

--- src/js/topIcons.js
+++ src/js/topIcons.js
@@ -29,7 +29,7 @@ const TopIcons = new Lang.Class({
   _createTray: function () {
     this._createIconsContainer();
 
-    tray = new Shell.TrayManager();
+    this._tray = new Shell.TrayManager();
     this._tray.connect('tray-icon-added', Lang.bind(this, this._addTrayIcon));
     this._tray.connect('tray-icon-removed', Lang.bind(this, this._removeTrayIcon));
     this._tray.manage_screen(global.screen, Main.Panel.actor);
