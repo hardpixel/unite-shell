3f496d479374242bb57f19dbec97d3b67df9e604
Jonian Guveli <jonian@hardpixel.eu>
2017-10-01 19:16:25 +0300

    build extension with new class modules

---
+++ dist/unite@hardpixel.eu/appMenu.js
@@ -0,0 +1,92 @@
+const Lang           = imports.lang;
+const Main           = imports.ui.main;
+const Mainloop       = imports.mainloop;
+const Shell          = imports.gi.Shell;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Unite          = ExtensionUtils.getCurrentExtension();
+const Helper         = Unite.imports.helperUtils;
+
+const AppMenu = new Lang.Class({
+  Name: 'AppMenu',
+  _appMenu: null,
+  _windowTracker: null,
+  _messageTray: null,
+  _sizeSignal: null,
+  _wmHandlerIDs: [],
+  _dsHandlerID: null,
+  _mtHandlerID: null,
+  _bbHandlerID: null,
+  _activeApp: null,
+  _activeWindow: null,
+
+  _init: function() {
+    this._appMenu       = Main.panel.statusArea.appMenu,
+    this._windowTracker = Shell.WindowTracker.get_default() || {},
+    this._messageTray   = Main.messageTray,
+    this._sizeSignal    = Helper.versionLT('3.24') ? 'size-change' : 'size-changed';
+
+    this._dsHandlerID = global.display.connect('notify::focus-window', Lang.bind(this, this._updateMenu));
+    this._mtHandlerID = this._messageTray.connect('source-removed', Lang.bind(this, this._restoreTitle));
+    this._bbHandlerID = this._messageTray._bannerBin.connect('notify::hover', Lang.bind(this, this._removeTitle));
+
+    this._wmHandlerIDs.push(global.window_manager.connect('destroy', Lang.bind(this, this._updateMenu)));
+    this._wmHandlerIDs.push(global.window_manager.connect(this._sizeSignal, Lang.bind(this, this._updateMenu)));
+
+    Mainloop.idle_add(Lang.bind(this, this._updateMenu));
+  },
+
+  _updateMenu: function () {
+    this._activeApp    = this._windowTracker.focus_app;
+    this._activeWindow = global.display.focus_window;
+
+    if (this._activeWindow && !this._activeWindow._updateTitleID) {
+      this._activeWindow._updateTitleID = this._activeWindow.connect('notify::title', Lang.bind(this, this._updateTitle));
+    }
+
+    this._updateTitle();
+  },
+
+  _updateTitle: function () {
+    let title = null;
+
+    if (this._activeWindow && this._activeWindow.get_maximized()) {
+      title = this._activeWindow.title;
+    }
+
+    if (this._activeApp && !title) {
+      title = this._activeApp.get_name();
+    }
+
+    if (title) {
+      this._appMenu._label.set_text(title);
+    }
+  },
+
+  _removeTitle: function () {
+    this._appMenu._label.set_text('');
+  },
+
+  _restoreTitle: function () {
+    Mainloop.idle_add(Lang.bind(this, this._updateMenu));
+  },
+
+  destroy: function() {
+    let windows = Helper.getAllWindows();
+
+    windows.forEach(function(win) {
+      if (win._updateTitleID) {
+        win.disconnect(win._updateTitleID);
+        win._updateTitleID = null;
+      }
+    });
+
+    global.display.disconnect(this._dsHandlerID);
+
+    this._messageTray.disconnect(this._mtHandlerID);
+    this._messageTray._bannerBin.disconnect(this._bbHandlerID);
+
+    this._wmHandlerIDs.forEach(function (handler) {
+      global.window_manager.disconnect(handler);
+    });
+  }
+});
---
+++ dist/unite@hardpixel.eu/helperUtils.js
@@ -0,0 +1,106 @@
+const Config = imports.misc.config;
+const Gio    = imports.gi.Gio;
+const Meta   = imports.gi.Meta;
+
+function getXWindow(win) {
+  try {
+    return win.get_description().match(/0x[0-9a-f]+/)[0];
+  } catch (err) {
+    return null;
+  }
+}
+
+function getAllWindows() {
+  let actors  = global.get_window_actors().map(function (w) { return w.meta_window; });
+  let windows = actors.filter(function(w) { return w.window_type !== Meta.WindowType.DESKTOP; });
+
+  return windows;
+}
+
+function getWindowButtons(return_only) {
+  let prefs  = new Gio.Settings({ schema_id: 'org.gnome.desktop.wm.preferences' });
+  let layout = prefs.get_string('button-layout');
+  let order  = layout.replace(/ /g, '').split(':');
+
+  if (order.length < 2) {
+    return;
+  }
+
+  let buttons  = collectWindowButtons(order[1].split(','));
+  let position = 'right';
+
+  if (!buttons) {
+    buttons    = collectWindowButtons(order[0].split(','));
+    position = 'left';
+  }
+
+  if (return_only == 'position') {
+    return position;
+  } else if (return_only == 'buttons') {
+    return buttons;
+  } else {
+    return [position, buttons];
+  }
+}
+
+function collectWindowButtons(layout_items) {
+  let names = ['close', 'minimize', 'maximize'];
+  let items = [];
+
+  layout_items.forEach(function (item) {
+    if (names.indexOf(item) > -1) {
+      items.push(item);
+    }
+  });
+
+  if (items.length == 0) {
+    items = null;
+  }
+
+  return items;
+}
+
+function versionCompare(v1, v2) {
+  let v1parts   = ('' + v1).split('.')
+  let v2parts   = ('' + v2).split('.')
+  let minLength = Math.min(v1parts.length, v2parts.length)
+
+  let i, p1, p2;
+
+  for (i = 0; i < minLength; i++) {
+    p1 = parseInt(v1parts[i], 10);
+    p2 = parseInt(v2parts[i], 10);
+
+    if (isNaN(p1)) { p1 = v1parts[i]; }
+    if (isNaN(p2)) { p2 = v2parts[i]; }
+
+    if (p1 === p2) {
+      continue;
+    } else if (p1 > p2) {
+      return 1;
+    } else if (p1 < p2) {
+      return -1;
+    } else {
+      return NaN;
+    }
+  }
+
+  if (v1parts.length === v2parts.length) {
+    return 0;
+  } else {
+    return (v1parts.length < v2parts.length) ? -1 : 1;
+  }
+}
+
+function versionEQ(ver) {
+  return versionCompare(Config.PACKAGE_VERSION, ver) === 0;
+}
+
+function versionLT(ver) {
+  return versionCompare(Config.PACKAGE_VERSION, ver) < 0;
+}
+
+function versionGT(ver) {
+  return versionCompare(Config.PACKAGE_VERSION, ver) > 0;
+}
+;
