bf98e4c96cddd9b33e4fd880f6b9fa9423930f38
Jonian Guveli <jonian@hardpixel.eu>
2017-10-01 19:15:56 +0300

    rewrite main extension file into class

--- src/js/extension.js
+++ src/js/extension.js
@@ -1,8 +1,52 @@
-//= require modules/main
-//= require modules/helpers
-//= require modules/show_window
-//= require modules/left_box
-//= require modules/buttons
-//= require modules/app_menu
-//= require modules/decoration
-//= require modules/top_icons
+const Lang             = imports.lang;
+const Main             = imports.ui.main;
+const ExtensionUtils   = imports.misc.extensionUtils;
+const Unite            = ExtensionUtils.getCurrentExtension();
+const ActivateWindow   = Unite.imports.activateWindow.ActivateWindow;
+const ExtendLeftBox    = Unite.imports.extendLeftBox.ExtendLeftBox;
+const AppMenu          = Unite.imports.appMenu.AppMenu;
+const WindowButtons    = Unite.imports.windowButtons.WindowButtons;
+const WindowDecoration = Unite.imports.windowDecoration.WindowDecoration;
+const TopIcons         = Unite.imports.topIcons.TopIcons;
+
+const UniteShell = new Lang.Class({
+  Name: 'UniteShell',
+  _activateWindow: null,
+  _extendLeftBox: null,
+  _appMenu: null,
+  _windowButtons: null,
+  _windowDecoration: null,
+  _topIcons: null,
+
+  _init: function() {
+    this._activateWindow   = new ActivateWindow();
+    this._extendLeftBox    = new ExtendLeftBox();
+    this._appMenu          = new AppMenu();
+    this._windowButtons    = new WindowButtons();
+    this._windowDecoration = new WindowDecoration();
+    this._topIcons         = new TopIcons();
+
+    Main.loadTheme();
+  },
+
+  destroy: function() {
+    this._activateWindow.destroy();
+    this._extendLeftBox.destroy();
+    this._appMenu.destroy();
+    this._windowButtons.destroy();
+    this._windowDecoration.destroy();
+    this._topIcons.destroy();
+
+    Main.loadTheme();
+  }
+});
+
+let uniteShell;
+
+function enable() {
+  uniteShell = new UniteShell();
+};
+
+function disable() {
+  uniteShell.destroy();
+};
