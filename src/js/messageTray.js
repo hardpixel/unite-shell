e899ec4db127a6b3a871caf381ce8bb059d6cbba
Jonian Guveli <jonian@hardpixel.eu>
2017-10-03 00:31:22 +0300

    move message tray to the right
    solves issue #12

---
+++ src/js/messageTray.js
@@ -0,0 +1,20 @@
+const Lang    = imports.lang;
+const Main    = imports.ui.main;
+const Clutter = imports.gi.Clutter;
+
+const MessageTray = new Lang.Class({
+  Name: 'MessageTray',
+  _container: null,
+
+  _init: function() {
+    this._container = Main.messageTray._bannerBin;
+
+    this._container.set_x_align(Clutter.ActorAlign.END);
+    this._container.set_width(390);
+  },
+
+  destroy: function() {
+    this._container.set_x_align(Clutter.ActorAlign.CENTER);
+    this._container.set_width(0);
+  }
+});
