11c3abf2fb7133ce5c577cce80561e67a75875f0
Jonian Guveli <jonian@hardpixel.eu>
2017-10-04 01:41:36 +0300

    add activities button module
    for now just hide the button

---
+++ src/js/activitiesButton.js
@@ -0,0 +1,16 @@
+const Lang = imports.lang;
+const Main = imports.ui.main;
+
+const ActivitiesButton = new Lang.Class({
+  Name: 'ActivitiesButton',
+  _activities: null,
+
+  _init: function() {
+    this._activities = Main.panel.statusArea.activities;
+    this._activities.actor.hide();
+  },
+
+  destroy: function() {
+    this._activities.actor.show();
+  }
+});
