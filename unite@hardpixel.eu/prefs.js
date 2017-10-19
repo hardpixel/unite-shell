955452afd153fedca3a4aa569a4aebf8da9a75f5
Jonian Guveli <jonian@hardpixel.eu>
2017-10-20 01:42:05 +0300

    convert prefs widget to class

--- unite@hardpixel.eu/prefs.js
+++ unite@hardpixel.eu/prefs.js
@@ -8,16 +8,35 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Unite          = ExtensionUtils.getCurrentExtension();
 const Convenience    = Unite.imports.convenience;
 
+var PrefsWidget = new GObject.Class({
+  Name: 'Unite.PrefsWidget',
+  GTypeName: 'PrefsWidget',
+  Extends: Gtk.Box,
+
+  _init: function(params) {
+    this.parent(params);
+
+    this._buildable = new Gtk.Builder();
+    this._buildable.add_from_file(Unite.path + '/settings.ui');
+
+    let prefsWidget = this._get_widget('prefs_widget');
+    this.add(prefsWidget);
+
+    this._settings = Convenience.getSettings();
+  },
+
+  _get_widget: function(name) {
+    return this._buildable.get_object(name);
+  }
+});
+
 function init() {
-  Convenience.initTranslations('unite');
+  Convenience.initTranslations();
 }
 
 function buildPrefsWidget() {
-  let buildable = new Gtk.Builder();
-  buildable.add_from_file(Unite.path + '/settings.ui');
+  let widget = new PrefsWidget();
+  widget.show_all();
 
-  let box = buildable.get_object('prefs_widget');
-  box.show_all();
-
-  return box;
+  return widget;
 }
