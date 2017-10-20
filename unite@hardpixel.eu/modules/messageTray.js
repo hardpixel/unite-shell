275ad72cd0add2f5c250a9379c10254c371bca0d
Olibia Tsati <olibia@hardpixel.eu>
2017-10-20 22:30:35 +0300

    implement notifications position setting

--- unite@hardpixel.eu/modules/messageTray.js
+++ unite@hardpixel.eu/modules/messageTray.js
@@ -1,15 +1,43 @@
-const Lang    = imports.lang;
-const Main    = imports.ui.main;
-const Clutter = imports.gi.Clutter;
+const Lang           = imports.lang;
+const Main           = imports.ui.main;
+const Clutter        = imports.gi.Clutter;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Unite          = ExtensionUtils.getCurrentExtension();
+const Convenience    = Unite.imports.convenience;
 
 var MessageTray = new Lang.Class({
   Name: 'Unite.MessageTray',
 
   _init: function() {
     this._container = Main.messageTray._bannerBin;
+    this._settings  = Convenience.getSettings();
 
-    this._container.set_x_align(Clutter.ActorAlign.END);
+    this._updatePosition();
     this._container.set_width(390);
+
+    this._connectSettings();
+  },
+
+  _connectSettings: function () {
+    this._settings.connect(
+      'changed::notifications-position',
+      Lang.bind(this, this._updatePosition)
+    );
+  },
+
+  _updatePosition: function () {
+    this._position = this._settings.get_enum('notifications-position');
+    this._alignMessages();
+  },
+
+  _alignMessages: function () {
+    let alignments = [
+      Clutter.ActorAlign.CENTER,
+      Clutter.ActorAlign.START,
+      Clutter.ActorAlign.END
+    ];
+
+    this._container.set_x_align(alignments[this._position]);
   },
 
   destroy: function() {
