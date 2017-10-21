4fbad5443501db71fb9f8db23df2d8aab2dda465
Olibia Tsati <olibia@hardpixel.eu>
2017-10-21 11:52:05 +0300

    optimize message tray module

--- unite@hardpixel.eu/modules/messageTray.js
+++ unite@hardpixel.eu/modules/messageTray.js
@@ -12,22 +12,32 @@ var MessageTray = new Lang.Class({
     this._container = Main.messageTray._bannerBin;
     this._settings  = Convenience.getSettings();
 
-    this._updatePosition();
-    this._container.set_width(390);
-
+    this._create();
     this._connectSettings();
   },
 
   _connectSettings: function () {
     this._settings.connect(
-      'changed::notifications-position',
-      Lang.bind(this, this._updatePosition)
+      'changed::notifications-position', Lang.bind(this, this._updatePosition)
     );
   },
 
   _updatePosition: function () {
     this._position = this._settings.get_enum('notifications-position');
     this._alignMessages();
+  },
+
+  _resetPosition: function () {
+    this._position = 0;
+    this._alignMessages();
+  },
+
+  _updateWidth: function () {
+    this._container.set_width(390);
+  },
+
+  _resetWidth: function () {
+    this._container.set_width(0);
   },
 
   _alignMessages: function () {
@@ -40,8 +50,13 @@ var MessageTray = new Lang.Class({
     this._container.set_x_align(alignments[this._position]);
   },
 
+  _create: function () {
+    this._updatePosition();
+    this._updateWidth();
+  },
+
   destroy: function() {
-    this._container.set_x_align(Clutter.ActorAlign.CENTER);
-    this._container.set_width(0);
+    this._resetPosition();
+    this._resetWidth();
   }
 });
