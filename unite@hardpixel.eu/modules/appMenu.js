14868b556ab796fba9244a778ae10bfa25293a9d
Jonian Guveli <jonian@hardpixel.eu>
2018-03-18 14:51:20 +0200

    fix appmenu always mode

--- unite@hardpixel.eu/modules/appMenu.js
+++ unite@hardpixel.eu/modules/appMenu.js
@@ -126,7 +126,7 @@ var AppMenu = new Lang.Class({
   _updateTitle: function () {
     let title = null;
     let maximized = Helpers.isMaximized(this._activeWindow, this._enabled);
-    let always = Helpers.getStateName(this._enabled) == 'always';
+    let always = Helpers.getStateName(this._enabled) == 'always' && this._activeWindow;
 
     if (always || maximized) {
       title = this._activeWindow.title;
