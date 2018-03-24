7b0f408cedbcbba8607170b8a4830cbf717def3c
Jonian Guveli <jonian@hardpixel.eu>
2018-03-24 11:28:40 +0200

    load desktop name module before activities button

--- unite@hardpixel.eu/extension.js
+++ unite@hardpixel.eu/extension.js
@@ -4,9 +4,9 @@ const ExtensionUtils   = imports.misc.extensionUtils;
 const Unite            = ExtensionUtils.getCurrentExtension();
 const ActivateWindow   = Unite.imports.modules.activateWindow.ActivateWindow;
 const ExtendLeftBox    = Unite.imports.modules.extendLeftBox.ExtendLeftBox;
+const DesktopName      = Unite.imports.modules.desktopName.DesktopName;
 const MessageTray      = Unite.imports.modules.messageTray.MessageTray;
 const ActivitiesButton = Unite.imports.modules.activitiesButton.ActivitiesButton;
-const DesktopName      = Unite.imports.modules.desktopName.DesktopName;
 const AppMenu          = Unite.imports.modules.appMenu.AppMenu;
 const WindowButtons    = Unite.imports.modules.windowButtons.WindowButtons;
 const WindowDecoration = Unite.imports.modules.windowDecoration.WindowDecoration;
@@ -18,9 +18,9 @@ var UniteShell = new Lang.Class({
   _init: function() {
     this._activateWindow   = new ActivateWindow();
     this._extendLeftBox    = new ExtendLeftBox();
+    this._desktopName      = new DesktopName();
     this._messageTray      = new MessageTray();
     this._activitiesButton = new ActivitiesButton();
-    this._desktopName      = new DesktopName();
     this._appMenu          = new AppMenu();
     this._windowButtons    = new WindowButtons();
     this._windowDecoration = new WindowDecoration();
@@ -32,9 +32,9 @@ var UniteShell = new Lang.Class({
   destroy: function() {
     this._activateWindow.destroy();
     this._extendLeftBox.destroy();
+    this._desktopName.destroy();
     this._messageTray.destroy();
     this._activitiesButton.destroy();
-    this._desktopName.destroy();
     this._appMenu.destroy();
     this._windowButtons.destroy();
     this._windowDecoration.destroy();
