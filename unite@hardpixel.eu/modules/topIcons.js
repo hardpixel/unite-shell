d64a69bd11347dc4401e2f5318775be0a5da018d
Jonian Guveli <jonian@hardpixel.eu>
2018-09-06 20:13:49 +0300

    topicons: gnome 3.30 support

--- unite@hardpixel.eu/modules/topIcons.js
+++ unite@hardpixel.eu/modules/topIcons.js
@@ -53,7 +53,12 @@ var TopIcons = new Lang.Class({
     this._tray = new Shell.TrayManager();
     this._tray.connect('tray-icon-added', Lang.bind(this, this._addTrayIcon));
     this._tray.connect('tray-icon-removed', Lang.bind(this, this._removeTrayIcon));
-    this._tray.manage_screen(global.screen, Panel.actor);
+
+    if (global.screen) {
+      this._tray.manage_screen(global.screen, Panel.actor);
+    } else {
+      this._tray.manage_screen(Panel.actor);
+    }
   },
 
   _destroyTray: function () {
