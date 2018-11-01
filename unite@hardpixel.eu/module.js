6c3adccaf1914eba4aa6beb8797598fb166471c5
Jonian Guveli <jonian@hardpixel.eu>
2018-11-01 20:05:07 +0200

    add base module class
    will be used as extend for all modules

---
+++ unite@hardpixel.eu/module.js
@@ -0,0 +1,42 @@
+const Lang            = imports.lang;
+const ExtensionUtils  = imports.misc.extensionUtils;
+const Unite           = ExtensionUtils.getCurrentExtension();
+const SignalsHandler  = Unite.imports.handlers.SignalsHandler;
+const SettingsHandler = Unite.imports.handlers.SettingsHandler;
+
+var BaseModule = new Lang.Class({
+  Name: 'Unite.BaseModule',
+
+  _init() {
+    this._signals  = new SignalsHandler(this);
+    this._settings = new SettingsHandler(this);
+
+    this._activate();
+    this._connectSettings();
+  },
+
+  _connectSettings() {},
+  _connnectSignals() {},
+  _onActivate() {},
+  _onDeactivate() {},
+
+  _activate() {
+    this._connnectSignals();
+    this._onActivate();
+  },
+
+  _deactivate() {
+    this._signals.disconnectAll();
+    this._onDeactivate();
+  },
+
+  _reload() {
+    this._deactivate();
+    this._activate();
+  },
+
+  destroy() {
+    this._deactivate();
+    this._settings.disconnectAll();
+  }
+});
