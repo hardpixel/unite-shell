db1682f1cc843400cff1b919dc50ab27fad489ea
Jonian Guveli <jonian@hardpixel.eu>
2018-11-03 14:32:44 +0200

    window buttons: refactor all functions

--- unite@hardpixel.eu/modules/windowButtons.js
+++ unite@hardpixel.eu/modules/windowButtons.js
@@ -40,57 +40,56 @@ var WindowButtons = new Lang.Class({
     this._unloadTheme();
   },
 
+  _createButton(action) {
+    if (!this._buttonsBox) return;
+
+    let style  = `window-button  ${action}`;
+    let button = new St.Button({ style_class: style, track_hover: true });
+    let icon   = new St.Bin({ style_class: 'icon' });
+
+    button._windowAction = action;
+
+    button.add_actor(icon);
+    button.connect('clicked', Lang.bind(this, this._onButtonClick));
+
+    this._buttonsBox.add(button);
+  },
+
   _createButtons() {
     [this._position, this._buttons] = Helpers.getWindowButtons();
+    if (!this._buttons || this._buttonsActor) return;
 
-    if (this._buttons && !this._buttonsActor) {
-      this._buttonsActor = new St.Bin();
-      this._buttonsBox   = new St.BoxLayout({ style_class: 'window-buttons-box' });
+    this._buttonsActor = new St.Bin();
+    this._buttonsBox   = new St.BoxLayout({ style_class: 'window-buttons-box' });
 
-      this._buttonsActor._windowButtons = true;
+    this._buttonsActor.add_actor(this._buttonsBox);
+    this._buttonsActor.hide();
 
-      this._buttonsActor.add_actor(this._buttonsBox);
-      this._buttonsActor.hide();
+    this._buttons.forEach(Lang.bind(this, this._createButton));
 
-      this._buttons.forEach(Lang.bind(this, function (action) {
-        let button = new St.Button({ style_class: 'window-button ' + action, track_hover: true });
-        let icon   = new St.Bin({ style_class: 'icon' });
-
-        button._windowAction = action;
-        button.add_actor(icon);
-        button.connect('clicked', Lang.bind(this, this._onButtonClick));
-
-        this._buttonsBox.add(button);
-      }));
-
-      if (this._position == 'left') {
-        let appmenu = Main.panel.statusArea.appMenu.actor.get_parent();
-        Main.panel._leftBox.insert_child_below(this._buttonsActor, appmenu);
-      }
-
-      if (this._position == 'right') {
-        Main.panel._rightBox.add_child(this._buttonsActor);
-      }
+    if (this._position == 'left') {
+      let appmenu = Main.panel.statusArea.appMenu.actor.get_parent();
+      Main.panel._leftBox.insert_child_below(this._buttonsActor, appmenu);
     }
+
+    if (this._position == 'right')
+      Main.panel._rightBox.add_child(this._buttonsActor);
   },
 
   _destroyButtons() {
-    if (this._buttonsBox) {
-      this._buttonsBox.destroy();
-      delete this._buttonsBox;
-    }
+    if (!this._buttonsActor) return;
 
-    if (this._buttonsActor) {
-      this._buttonsActor.destroy();
-      delete this._buttonsActor;
-    }
+    this._buttonsActor.destroy();
+
+    this._buttonsBox   = null;
+    this._buttonsActor = null;
   },
 
   _updateButtons() {
-    if (this._buttonsActor) {
-      this._destroyButtons();
-      this._createButtons();
-    }
+    if (!this._buttonsActor) return;
+
+    this._destroyButtons();
+    this._createButtons();
   },
 
   _updateTheme() {
@@ -99,69 +98,63 @@ var WindowButtons = new Lang.Class({
   },
 
   _loadTheme() {
-    let context = St.ThemeContext.get_for_stage(global.stage).get_theme();
+    if (this._buttonsTheme || !this._buttonsBox) return;
+
     let theme   = this._settings.get('window-buttons-theme');
     let cssPath = GLib.build_filenamev([Unite.path, 'themes', theme, 'stylesheet.css']);
+    if (!GLib.file_test(cssPath, GLib.FileTest.EXISTS)) return;
 
-    if (GLib.file_test(cssPath, GLib.FileTest.EXISTS)) {
-      let cssFile = Gio.file_new_for_path(cssPath);
+    this._buttonsTheme = Gio.file_new_for_path(cssPath);
 
-      if (!this._buttonsTheme) {
-        this._buttonsTheme = cssFile;
-        context.load_stylesheet(this._buttonsTheme);
+    let context = St.ThemeContext.get_for_stage(global.stage).get_theme();
+    context.load_stylesheet(this._buttonsTheme);
 
-        if (this._buttonsBox) {
-          this._buttonsBox.add_style_class_name(theme + '-buttons');
-        }
-      }
-    }
+    this._buttonsBox.add_style_class_name(`${theme}-buttons`);
   },
 
   _unloadTheme() {
-    if (this._buttonsTheme) {
-      let theme   = this._settings.get('window-buttons-theme');
-      let context = St.ThemeContext.get_for_stage(global.stage).get_theme();
-      context.unload_stylesheet(this._buttonsTheme);
+    if (!this._buttonsTheme) return;
 
-      if (this._buttonsBox) {
-        this._buttonsBox.remove_style_class_name(theme + '-buttons');
-      }
+    let context = St.ThemeContext.get_for_stage(global.stage).get_theme();
+    context.unload_stylesheet(this._buttonsTheme);
 
-      delete this._buttonsTheme;
+    if (this._buttonsBox) {
+      let theme = this._settings.get('window-buttons-theme');
+      this._buttonsBox.remove_style_class_name(`${theme}-buttons`);
     }
+
+    delete this._buttonsTheme;
   },
 
   _onButtonClick(actor, event) {
-    if (this._activeWindow) {
-      switch (actor._windowAction) {
-        case 'minimize':
-          this._minimizeWindow();
-          break;
-        case 'maximize':
-          this._maximizeWindow();
-          break;
-        case 'close':
-          this._closeWindow();
-          break;
-      }
+    if (!this._activeWindow) return;
+
+    switch (actor._windowAction) {
+      case 'minimize':
+        this._minimizeWindow();
+        break;
+      case 'maximize':
+        this._maximizeWindow();
+        break;
+      case 'close':
+        this._closeWindow();
+        break;
     }
   },
 
   _minimizeWindow() {
-    if (!this._activeWindow.minimized) {
+    if (!this._activeWindow.minimized)
       this._activeWindow.minimize();
-    }
   },
 
   _maximizeWindow() {
     let bothMaximized = Meta.MaximizeFlags.BOTH;
     let maximizeState = this._activeWindow.get_maximized();
 
-    if (maximizeState === bothMaximized) {
+    if (maximizeState === bothMaximized)
       this._activeWindow.unmaximize(bothMaximized);
-    } else {
+    else
       this._activeWindow.maximize(bothMaximized);
-    }
   },
 
   _closeWindow() {
@@ -170,26 +163,24 @@ var WindowButtons = new Lang.Class({
 
   _toggleButtons() {
     this._activeWindow = global.display.focus_window;
+    if (!this._buttonsActor) return;
 
-    if (this._buttonsActor) {
-      let valid    = Helpers.isValidWindow(this._activeWindow);
-      let overview = Main.overview.visibleTarget;
-      let target   = Main.panel.statusArea.appMenu._targetApp;
-      let running  = target != null && target.get_state() == Shell.AppState.RUNNING;
-      let visible  = running && !overview;
+    let overview = Main.overview.visibleTarget;
+    let valid    = Helpers.isValidWindow(this._activeWindow);
+    let visible  = false;
 
-      if (!overview && valid) {
-        let maximized = Helpers.isMaximized(this._activeWindow, this._enabled);
-        let always    = this._enabled == 'always' && this._activeWindow;
+    if (!overview && valid) {
+      let maxed   = Helpers.isMaximized(this._activeWindow, this._enabled);
+      let always  = this._enabled == 'always' && this._activeWindow;
 
-        visible = always || maximized;
-      }
+      visible = always || maxed;
+    } else {
+      let target  = Main.panel.statusArea.appMenu._targetApp;
+      let running = target != null && target.get_state() == Shell.AppState.RUNNING;
 
-      if (visible) {
-        this._buttonsActor.show();
-      } else {
-        this._buttonsActor.hide();
-      }
+      visible = running && !overview;
     }
+
+    visible ? this._buttonsActor.show() : this._buttonsActor.hide()
   }
 });
