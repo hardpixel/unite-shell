309b1c8a7fa5ec07470a561b5fa30307aad68bdf
jonian <jonian@hardpixel.eu>
2021-03-25 17:21:23 +0200

    style handler: create file directory if not exists

--- unite@hardpixel.eu/handlers.js
+++ unite@hardpixel.eu/handlers.js
@@ -34,9 +34,12 @@ function getFileContents(path) {
 }
 
 function setFileContents(path, contents) {
-  if (fileExists(path)) {
-    GLib.file_set_contents(path, contents)
+  if (!fileExists(path)) {
+    const dirname = GLib.path_get_dirname(path)
+    GLib.mkdir_with_parents(dirname, parseInt('0700', 8))
   }
+
+  GLib.file_set_contents(path, contents)
 }
 
 function resetGtkStyles(filepath) {
