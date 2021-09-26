b86f20da178e428b1cb541c1b81573c8737ac3e0
Nelson Ferreira <nelson.ferreira@ieee.org>
2021-09-26 15:21:32 -0400

    Also check the disconnect handler to really fix the error on suspend

--- unite@hardpixel.eu/handlers.js
+++ unite@hardpixel.eu/handlers.js
@@ -105,7 +105,7 @@ var Signals = class Signals {
   disconnect(key) {
     if (this.hasSignal(key)) {
       const data = this.signals.get(key)
-      if (data.object != null) {
+      if (data.object  && data.object.signal_handler_disconnect) {
         data.object.disconnect(data.signalId)
       }
 
